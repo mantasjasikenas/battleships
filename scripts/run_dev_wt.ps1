42281f2ababbfffc21eac2c804f238895095d3fb
Mantelis <mantasjasikenas@gmail.com>
2024-09-16 11:18:51 +0300

    add run scripts for Windows Terminal

---
+++ scripts/run_dev_wt.ps1
@@ -0,0 +1,11 @@
+# Script to open a new Windows Terminal window with 3 panes, one for the root folder, one for the backend folder and one for the frontend folder.
+
+$script_path = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$root_path = Split-Path -Parent $script_path
+$backend_path = Join-Path $root_path "src\battleships-api"
+$frontend_path = Join-Path $root_path "src\battleships-ui"
+
+$frontend_command = "npm ci && npm start"
+$backend_command = "dotnet watch"
+
+wt.exe new-tab --title battleships pwsh -wd $root_path `; split-pane --title backend -H pwsh -wd $backend_path -Command $backend_command `; split-pane --title frontend -V pwsh -wd $frontend_path -Command $frontend_command
